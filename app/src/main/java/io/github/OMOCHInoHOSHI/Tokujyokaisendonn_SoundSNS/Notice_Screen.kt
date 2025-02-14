025951122231984a2c5b40bd95d80b5c857a2d46
OMOCHInoHOSHI <166191100+OMOCHInoHOSHI@users.noreply.github.com>
2025-02-14 14:47:18 +0900

    通知画面(仮)

--- app/src/main/java/io/github/OMOCHInoHOSHI/Tokujyokaisendonn_SoundSNS/Notice_Screen.kt
+++ app/src/main/java/io/github/OMOCHInoHOSHI/Tokujyokaisendonn_SoundSNS/Notice_Screen.kt
@@ -1,2 +1,26 @@
 package io.github.OMOCHInoHOSHI.Tokujyokaisendonn_SoundSNS
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import io.github.OMOCHInoHOSHI.Tokujyokaisendonn_SoundSNS.ui.theme.VeilTheme
+
+
+// 通知画面
+@Preview(showBackground = true)
+@Composable
+fun NoticeScreen(){
+    Scaffold(){innerPadding ->
+        Box(modifier = Modifier.padding(innerPadding))
+        Box(){
+            Text("通知")
+        }
+    }
+
+
+}
+
